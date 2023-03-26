7af31f51650265e372ffc479c7fcbd3c9a9c1640
Ryan Stewart <ryanstew17@gmail.com>
2023-03-26 12:11:36 -0300

    odd returns false assignment

--- count-down.js
+++ count-down.js
@@ -1,4 +1,24 @@
-let x = 60
-while x >= 0;
-    print(x);
-    x = x - 1; 
+timeInSeconds =60;
+
+while (timeInSeconds >= 0) {
+    if (timeInSeconds === 50) {
+        console.log("Orbiter transfers from ground to internal power")
+    }
+    else if (timeInSeconds === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start")
+    }
+    else if (timeInSeconds === 16) {
+        console.log("Activate launch pad sound suppression system")
+    }
+    else if (timeInSeconds === 10) {
+        console.log("Activate main engine hydrogen burnoff system")
+    }
+    else if (timeInSeconds === 6) {
+        console.log("Main engine start")
+    }
+    else if (timeInSeconds === 0) {
+        console.log("Solid rocket booster ignition and liftoff!")
+    }
+    else console.log("T-" + timeInSeconds + " seconds");
+    timeInSeconds -=1;
+}
---
+++ odd.js
@@ -0,0 +1,10 @@
+var isOdd = function(number) {
+
+  if (number % 2 === 0) { 
+    return true; 
+  } else { 
+    return false; }
+  };
+  isOdd();
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
