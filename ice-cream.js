e007338baf7e72ef564dc109141bc9589644a78a
Ryan Stewart <ryanstew17@gmail.com>
2023-03-27 15:29:22 -0300

    adding new files

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("rootbeer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
